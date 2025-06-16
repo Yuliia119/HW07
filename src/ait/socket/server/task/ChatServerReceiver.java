6fed0f335d7cbfebad845320a4bbcb6d0bfca0e6
Yuliia <uliagrinenko1981@gmail.com>
2025-06-16 23:15:25 +0200

    creat Socket Server

--- src/ait/socket/server/task/ChatServerReceiver.java
+++ src/ait/socket/server/task/ChatServerReceiver.java
@@ -1,9 +1,12 @@
 package ait.socket.server.task;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 
-public class ChatServerReceiver implements Runnable{
+public class ChatServerReceiver implements Runnable {
     private final Socket socket;
     private final BlockingQueue<String> messageBox;
 
@@ -14,6 +17,15 @@ public class ChatServerReceiver implements Runnable{
 
     @Override
     public void run() {
-
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            String message;
+            while ((message = reader.readLine()) != null) {
+                messageBox.put(message);
+            }
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Client disconnected: " + socket.getInetAddress() + ":" + socket.getPort());
+        }
     }
 }
+// BufferedReader - объект прочитает входящие строки от клиента
+// messageBox.put(message) - отправляет каждое сообщение в очередь для отправки другим клиентам
